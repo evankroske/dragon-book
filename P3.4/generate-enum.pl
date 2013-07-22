6113db3cd9c32d50d93eebe05dd9d921e8d2eaeb
Evan Kroske <evan@evankroske.com>
2013-07-22 15:23:14 -0500

    Fixed bug in enum definition and made lexer display tokens inline.

--- P3.4/generate-enum.pl
+++ P3.4/generate-enum.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 use feature qw(say);
 
-my @tokens = qw(ID OPEN_TAG_OPEN OPEN_TAG_CLOSE CLOSE_TAG_NONEMPTY CLOSE_TAG_EMPTY OPEN_SCRIPTLET OPEN_EXPRESSION OPEN_DIRECTIVE OPEN_DECLARATION SCOPE);
+my @tokens = qw(NONE ID OPEN_TAG_OPEN OPEN_TAG_CLOSE CLOSE_TAG_NONEMPTY CLOSE_TAG_EMPTY OPEN_SCRIPTLET OPEN_EXPRESSION OPEN_DIRECTIVE OPEN_DECLARATION CLOSE_JSP SCOPE);
 
 say('typedef enum {' . join(', ', @tokens) . '} token;');
 say('char *token_to_string[] = {' . join(', ', map("\"$_\"", @tokens)) . '};');
